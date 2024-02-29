fb2f25601033e3354849883b1e1302e43c5b64d0
trhieung <nthieu.ngtr@gmail.com>
2024-02-29 18:04:33 +0700

    setting

--- transformers/views.py
+++ transformers/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+import sqlite3
 
 # Create your views here.
 
 def index(request):
+    print(sqlite3.sqlite_version)
     # return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, "index.html")
